b87c50ade7f1736b409ab93070b2d37d213ecc8f
Pedro Pontes <pmiguelpontes@outlook.com>
2024-02-06 17:30:53 -0300

    add descriptografar

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 function criptografar(){
 
     let texto = document.getElementById('textarea').value;
@@ -19,5 +18,19 @@ function criptografar(){
 }
 
 function descriptografar(){
+
+    let texto = document.getElementById('textarea').value;
+
+    let textoDescriptografado = texto
+    .replace(/ai/g, "a")
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+
+    document.getElementById('conteudo__area__texto').style.display = 'none';
+
+    let campoMostrarTexto = document.getElementById('conteudo__area__criptografia');
+    campoMostrarTexto.innerHTML = textoDescriptografado;
     
 }
